b0f485a81168693f58e3d52a4fe7d0aab795530f
ken <ken930526@gmail.com>
2025-04-01 01:14:22 +0800

    增加米字號

---
+++ 第一題.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>    
+#include <conio.h>
+int main() {
+    printf("********************************************\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("    (\\_/)\n");
+    printf("     (o.o)                 \n");
+    printf("     (> <)                                 \n");
+    printf("                                        \n");
+    printf("               \n");
+    printf("                                          \n");
+    printf("                        (T_T)\n");
+    printf("\n");
+    printf("          ( -_-)\n");
+    printf("\n");
+    printf("\n");
+    printf("(¡Ù3¡Ø)\n");
+    printf("\n");
+    printf("\n");
+    printf("********************************************\n");
+    getch();   
+    system("cls"); 
+
+    return 0;
+}
